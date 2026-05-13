1b1541d5f20edf0be54fe2f0abe2edd89d8c2aa7
Gurpreet Chhabra <gurpreet@nybian.com>
2026-05-13 13:24:07 +0530

    first setup commit

--- config/s3.js
+++ config/s3.js
@@ -10,8 +10,8 @@ const s3 = new S3Client({
 console.log('S3 Client initialized successfully', process.env.AWS_REGION);
 
 // AWS_REGION=us-east-1
-// AWS_ACCESS_KEY=AKIAWEAII5CTJ25DH5CO
-// AWS_SECRET_KEY=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
+// AWS_ACCESS_KEY=xxxx
+// AWS_SECRET_KEY=xxx
 // AWS_BUCKET_NAME=vijay-file-upload-bucket
 
 module.exports = s3;
